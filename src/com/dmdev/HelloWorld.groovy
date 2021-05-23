acdd72a905d02bbd4d4494dc77b23bda08a731ae
Dzianis Matveyenka <dmatveyenka@fitbit.com>
2021-05-23 22:19:24 +0300

    added files for lesson-4

--- src/com/dmdev/HelloWorld.groovy
+++ src/com/dmdev/HelloWorld.groovy
@@ -1,11 +1,19 @@
 package com.dmdev
 
+import java.sql.DriverManager
+
 class HelloWorld {
 
     Integer value
 
 //    @PackageScope
     static void main(String[] args) {
+        String value = "Hello World"
+        Class<String> clazz = value.getClass()
+        assert clazz == String.class
         println "Hello World"
+        println clazz.getClassLoader()
+        println DriverManager.class.getClassLoader()
+        println HelloWorld.class.getClassLoader()
     }
 }
