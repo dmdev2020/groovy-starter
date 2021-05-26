1de70961fcf6733ece082b97183fe6bf12b8aa15
Dzianis Matveyenka <dmatveyenka@fitbit.com>
2021-05-26 09:32:06 +0300

    added files for lesson-5

---
+++ src/com/dmdev/lesson5/VariablesRunner.groovy
@@ -0,0 +1,26 @@
+package com.dmdev.lesson5
+
+import groovy.transform.CompileStatic
+
+//import java.sql.Date
+//import java.sql.Date as SqlDate
+
+class VariablesRunner {
+
+//    def field
+
+    /**
+     * byte, short, int, long, BigInteger
+     * float, double, BigDecimal
+     * char
+     * boolean
+     */
+    @CompileStatic
+    static void main(def args) {
+//        int value = 5
+        def value = 5
+        def value2 = 36G
+        def value3 = 3.3G
+//        new SqlDate()
+    }
+}
