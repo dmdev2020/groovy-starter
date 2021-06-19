7d5231a3854efcdeb439185672baea97e265e112
Dzianis Matveyenka <dmatveyenka@fitbit.com>
2021-06-19 15:17:16 +0300

    added files for lesson-20

--- src/com/dmdev/lesson17/Student.groovy
+++ src/com/dmdev/lesson17/Student.groovy
@@ -1,10 +1,18 @@
 package com.dmdev.lesson17
 
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.Immutable
 import groovy.transform.ToString
 import groovy.transform.TupleConstructor
+import groovy.transform.builder.Builder
 
 @ToString
 @TupleConstructor
+@EqualsAndHashCode
+//@Canonical
+@Immutable
+@Builder
+//@Slf4j
 //@Mixin(WithId.class)
 class Student implements WithId {
     String firstName
@@ -13,5 +21,12 @@ class Student implements WithId {
 
     def getAt(Integer index) {
         index == 0 ? firstName : lastName
+    }
+
+    static void main(String[] args) {
+//        Student.builder()
+//            .lastName("Ivanov")
+//            .firstName("Ivan")
+//            .build()
     }
 }
